27f757a21ef44986af4beb26773b7c241b996691
Branimir Butković <butkovic@uhp.digital>
2022-07-15 20:58:10 +0200

    Removed twig global

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,22 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-//    /**
-//     * @Route({
-//     *     "hr": "/",
-//     *     "en": "/homepage",
-//     *     "it": "/prima-pagina",
-//     *     "de": "/titleseite"
-//     *  }, name="homepage")
-//     */
     #[Route("/", name: "homepage")]
-    public function indexAction(Request $request): Response
+    public function indexAction(): Response
     {
         return $this->render('home/index.html.twig');
     }
